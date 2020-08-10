0c20a4a3b96391c1bdf12c5622cbbeecdeebd862
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-08-10 15:53:47 -0400

    created shared header in php file

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Matthew Conforti</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&family=Roboto&display=swap">
+        <link rel="stylesheet" href="styles/homestyle.css">
+    </head>
+    <body>
+        <?php
+            //include the header
+            include('includes/header.php');
+        ?>
+        <hr>
+        <div id="grid_container">
+            <div class="grid_item">
+                <h2>Intro</h2>
+                <p>This is some content</p>
+            </div>
+            <div class="grid_item">
+                <h2>About</h2>
+                <p>This is some content</p>
+            </div>
+            <div class="grid_item">
+                <h2>Technical Skills</h2>
+                <p>This is some content</p>
+            </div>
+        </div>
+        <div id="footer">
+            <a href="#top">back to top</a>
+        </div>
+    </body>
+</html>
